ad9fa11c890d18a7023bc03fbe259277b2237a09
Raju Shahi <raju86shahi@gmail.com>
2021-10-27 19:53:53 +1100

    Added web service

---
+++ flask_web/app.py
@@ -0,0 +1,14 @@
+# flask_web/app.py
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello Omniscient!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
